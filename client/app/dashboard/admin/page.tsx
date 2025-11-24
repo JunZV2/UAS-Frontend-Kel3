4b27075c11239f402018c1873f07cbbe310643a0
JunZV2 <ferijun2006@gmail.com>
2025-11-24 08:06:38 +0700

    update dashboard admin

--- client/app/dashboard/admin/page.tsx
+++ client/app/dashboard/admin/page.tsx
@@ -54,7 +54,7 @@ type Message = {
   tanggal: string;
 };
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_URL = 'https://uas-frontend-kel3-api.onrender.com';
 
 type ProductFormProps = {
   formState: {
@@ -904,4 +904,5 @@ export default function AdminDashboardPage() {
   );
 
 }
+
 
